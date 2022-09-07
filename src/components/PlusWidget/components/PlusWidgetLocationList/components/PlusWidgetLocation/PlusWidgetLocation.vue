4c1cba0e6df2f47d4095338296439119b931bfeb
lokko <lokkomokko1@gmail.com>
2022-09-07 22:29:00 +0300

    process

--- src/components/PlusWidget/components/PlusWidgetLocationList/components/PlusWidgetLocation/PlusWidgetLocation.vue
+++ src/components/PlusWidget/components/PlusWidgetLocationList/components/PlusWidgetLocation/PlusWidgetLocation.vue
@@ -29,7 +29,7 @@
           </div>
           <div class="plus-widget-location-weather-info__item">
             <p>
-              Dev point:
+              Dew point:
               {{
                 calcDewPoint(
                   weatherInfo.main.temp.toFixed(0),
